068b43701e3e210eee5be272a6d98861d1fabc6c
Tim Robertson <timrobertson0122@gmail.com>
2015-05-05 22:08:09 +0100

    Update plane.rb
    Updating with comments following review.

--- lib/plane.rb
+++ lib/plane.rb
@@ -2,13 +2,13 @@
 require_relative 'airport'
 
 class Plane
-  attr_accessor :status
+  attr_accessor :status # why setter?
   def initialize
     @status = 'flying'
   end
 
   def takeoff
-    @status = 'flying'
+    @status = 'flying' # is this doing much?
   end
 
   def land
